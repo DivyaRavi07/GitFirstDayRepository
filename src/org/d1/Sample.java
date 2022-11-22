cfe89b2bdfb81bcdcd19baf91b5efd36e6a13a23
DivyaRavi07 <divyaravi989@gmail.com>
2022-11-22 19:20:26 +0530

    Few Lines of Code Storing in a Remote Area

---
+++ src/org/d1/Sample.java
@@ -0,0 +1,14 @@
+package org.d1;
+
+public class Sample {
+	private void computerModel() {
+		System.out.println("Model Name:HP");
+
+	}
+	public static void main(String[] args) {
+		Sample c = new  Sample();
+		c.computerModel();
+	}
+	}
+
+
